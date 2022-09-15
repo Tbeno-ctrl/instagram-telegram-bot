64e361402af985544e82fdb2532384f3f83bb31e
Tbeno-ctrl <84532920+Tbeno-ctrl@users.noreply.github.com>
2022-09-16 01:01:05 +0300

    Update sample-config.py

--- sample-config.py
+++ sample-config.py
@@ -1,9 +1,9 @@
-TOKEN = 'YOUR-BOT-TOKEN'
-admin_id = 0
-helper_id = 0
+TOKEN = '1087816990:AAENQyz_AQygCqpIyayWIsr8auMgbCM-Uis'
+admin_id = 1095218250
+helper_id = 1095218250
 
 ##### upload_file.py config #####
-api_id = 0
-api_hash = 'api_hash'
-name = 'name'
-bot_username = 'bot_username'
+api_id = 1982654
+api_hash = '449564ef23cc55c50763310cde6c31a4'
+name = 'beno'
+bot_username = 'VVKVBOT'
